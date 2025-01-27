96f7773a969e513ba392ebfaa237399ad676ba8b
codms-15 <150774523+codms0105@users.noreply.github.com>
2025-01-27 22:00:06 +0900

    home main

--- app/src/main/java/com/example/guru2_10/MainActivity.kt
+++ app/src/main/java/com/example/guru2_10/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.guru2_10
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // 분리수거 챌린지 버튼 클릭 시 챌린지 화면으로 이동
+        val btnChallenge: Button = findViewById(R.id.btn_challenge)
+        btnChallenge.setOnClickListener {
+            val intent = Intent(this, ChallengeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
+
+//다른 버튼 이동 코드 필요함
