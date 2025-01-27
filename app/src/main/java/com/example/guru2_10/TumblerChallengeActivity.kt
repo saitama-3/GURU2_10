da25130d6e9595bddfb284e7255cb3f09e8926f9
codms-15 <150774523+codms0105@users.noreply.github.com>
2025-01-27 21:52:19 +0900

    Update TumblerChallengeActivity.kt

--- app/src/main/java/com/example/guru2_10/TumblerChallengeActivity.kt
+++ app/src/main/java/com/example/guru2_10/TumblerChallengeActivity.kt
@@ -1,1 +1,24 @@
+package com.example.guru2_10
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
+class TumblerChallengeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tumbler_challenge) // activity_tumbler_challenge.xml 연결
+
+        // 인증하기 버튼 설정
+        val btnCertify: Button = findViewById(R.id.btn_certify)
+
+        // 버튼 클릭 이벤트 처리
+        btnCertify.setOnClickListener {
+            // 인증 화면으로 이동
+            val intent = Intent(this, CertificationCompleteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
