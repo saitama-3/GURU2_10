49ff9d4813e0a29065c93add2e2fd1c89f75bc1f
hyeona <144097639+5492-7524@users.noreply.github.com>
2025-01-29 17:14:08 +0900

    challenge home

--- app/src/main/java/com/example/guru2_10/ChallengeActivity.kt
+++ app/src/main/java/com/example/guru2_10/ChallengeActivity.kt
@@ -17,6 +17,21 @@ class ChallengeActivity : AppCompatActivity() {
             val intent = Intent(this, TumblerChallengeActivity::class.java)
             startActivity(intent)
         }
+        
+        // 분리수거 챌린지 버튼 클릭 시 이동
+        val btnRecycle: Button = findViewById(R.id.btn_recycling_challenge)
+        btnRecycle.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        //퀴즈 버튼 클릭 시 이동
+        val btnQuiz: Button = findViewById(R.id.btn_quiz)
+        btnQuiz.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+        }
+        
         // 홈으로 가기 버튼 클릭 시 메인 화면(MainActivity)으로 이동
         val btnHome: Button = findViewById(R.id.btn_home)
         btnHome.setOnClickListener {
