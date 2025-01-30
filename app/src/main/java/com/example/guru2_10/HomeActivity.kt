6e6651ad14494b6323ab58fe051c56850428ca13
chaelim <chaelimi239@gmail.com>
2025-01-31 01:09:51 +0900

    guide

--- app/src/main/java/com/example/guru2_10/HomeActivity.kt
+++ app/src/main/java/com/example/guru2_10/HomeActivity.kt
@@ -1,22 +1,37 @@
 package com.example.guru2_10
 
+
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 
-class homeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home) // XML 파일 연결
+        setContentView(R.layout.activity_main) // XML 연결
 
-        // button2와 연결
-        val button = findViewById<Button>(R.id.hButton2)
-        button.setOnClickListener {
-            // MainActivity로 이동
-            val intent = Intent(this, guidehomeActivity::class.java)
-            startActivity(intent)
+        // 버튼 연결
+        val mapButton: Button = this.findViewById(R.id.hButton3)
+        val guideButton: Button = this.findViewById(R.id.hButton2)
+        val myPageButton: Button = this.findViewById(R.id.hButton1)
+        val challengeButton: Button = this.findViewById(R.id.hButton)
+
+        // 버튼 클릭 이벤트
+        mapButton.setOnClickListener {
+            startActivity(Intent(this, MapActivity::class.java))
+        }
+
+        guideButton.setOnClickListener {
+            startActivity(Intent(this, guidehomeActivity::class.java))
+        }
+
+        myPageButton.setOnClickListener {
+            startActivity(Intent(this, MypageActivity::class.java))
+        }
+
+        challengeButton.setOnClickListener {
+            startActivity(Intent(this, ChallengeActivity::class.java))
         }
     }
 }
-
