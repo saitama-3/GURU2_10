46cffbfdd2cea7d2865093e2260fe1461b6cb212
gintama1111 <ww3269@naver.com>
2025-01-31 17:49:06 +0900

    mlkit,camarax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,12 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.1")  // OkHttp 라이브러리 추가
     implementation("com.squareup.okhttp3:okhttp-urlconnection:4.9.1") // URLConnection을 사용할\ 경우 추가
     implementation ("androidx.recyclerview:recyclerview:1.2.1")
+
+
+    implementation ("com.google.mlkit:text-recognition:16.0.1") // Google ML Kit 텍스트 인식 라이브러리
+    implementation ("androidx.camera:camera-core:1.4.1") // CameraX 핵심 라이브러리
+    implementation ("androidx.camera:camera-camera2:1.4.1") // Camera2 구현을 위한 CameraX 라이브러리
+    implementation ("androidx.camera:camera-lifecycle:1.4.1") // CameraX 라이브러리와 Lifecycle 연동을 위한 라이브러리
+    implementation ("androidx.camera:camera-view:1.4.1") // CameraX 뷰 클래스를 위한 라이브러리
+    
 }
